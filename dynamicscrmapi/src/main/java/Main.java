e9f042b3b8e6845a96578d9647ead3e45fb93213
Greg Degruy <gregory.degruy@gmail.com>
2019-04-19 20:55:17 -0700

    + final todo 4 complete ! 3 and 6 need feedback from prod team

--- dynamicscrmapi/src/main/java/Main.java
+++ dynamicscrmapi/src/main/java/Main.java
@@ -47,10 +47,11 @@ public class Main {
             // TODO: 3
             // readEntityAuditHistory(accessToken);
             // TODO: 4
+            getAssociatedAccountAddresses(accessToken);
             // TODO: 5
             // createWithDataReturned(accessToken);
             // TODO: 6
-            bacthAccountPost(accessToken);
+            // bacthAccountPost(accessToken);
         }
         catch (MalformedURLException e) { }
         catch (InterruptedException e) { }
@@ -59,17 +60,6 @@ public class Main {
     }
 
     // TODO: 1
-    // Add Option set values dynamically using OData Web API.
-    // GET all GlobalOptionSetDefinitions and find the MetadataId of the one you
-    // want to insert an option into
-    // https://msott.api.crm.dynamics.com/api/data/v9.0/GlobalOptionSetDefinitions
-    // You can use the MetadataId to see all current options for your option set
-    // https://msott.api.crm.dynamics.com/api/data/v9.0/GlobalOptionSetDefinitions(06d1a507-4d57-e911-a82a-000d3a1d5203)/Microsoft.Dynamics.CRM.OptionSetMetadata/Options
-    // if some API calls work and others don't, most likely the user does not have the right permissions
-    // SecLib::CheckPrivilege failed. User: b4cd9bac-8851-e911-a825-000d3a1d5de8,
-    // PrivilegeName: prvWriteOptionSet, PrivilegeId: 2493b394-f9d7-4604-a6cb-13e1f240450d,
-    // Required Depth: Basic, BusinessUnitId: 1abfdddc-8140-e911-a823-000d3a1a25b8,
-    // MetadataCache Privileges Count: 2999, User Privileges Count: 682
     public static void addOptionSetValuesDynamically(String accessToken) {
         int previousValue = 0;
         String optionSetGuidString = "06d1a507-4d57-e911-a82a-000d3a1d5203";
@@ -78,7 +68,7 @@ public class Main {
             OkHttpClient client = new OkHttpClient();
 
             Request request = new Request.Builder()
-                    .url("https://msott.api.crm.dynamics.com/api/data/v9.0/GlobalOptionSetDefinitions%28" +
+                    .url(REST_API_URL + "GlobalOptionSetDefinitions%28" +
                             optionSetGuidString +
                             "%29/Microsoft.Dynamics.CRM.OptionSetMetadata/Options")
                     .get()
@@ -97,7 +87,7 @@ public class Main {
 
         String optionSetName = "new_msdatzooptionset";
         String value = Integer.toString(++previousValue);
-        String label = "changeMe";
+        String label = "newOptionLabel";
         String metadataId = "06d1a507-4d57-e911-a82a-000d3a1d5203";
 
         try {
@@ -144,7 +134,7 @@ public class Main {
                     "}" +
                 "}");
             Request request = new Request.Builder()
-                    .url("https://msott.api.crm.dynamics.com/api/data/v9.0/InsertOptionValue")
+                    .url(REST_API_URL + "InsertOptionValue")
                     .post(body)
                     .addHeader("OData-MaxVersion", "4.0")
                     .addHeader("OData-Version", "4.0")
@@ -161,14 +151,6 @@ public class Main {
     }
 
     // TODO: 2
-    // Create custom party list using OOTB Party List (Field to select Multiple lookup values):
-    // Functional Scenario:
-    // We have Account entity and Contact entity.
-    // We need to select multiple Contacts for single Account.
-    // Like Email Entity To, BCC, CC fields.
-    // https://docs.microsoft.com/en-us/dynamics365/customer-engagement/web-api/activityparty?view=dynamics-ce-odata-9
-    // participationtypemask defines the role on the email
-    // https://community.dynamics.com/crm/f/117/t/174608
     public static void createEmailWithPartyList(String accessToken) {
         try {
             OkHttpClient client = new OkHttpClient();
@@ -229,11 +211,6 @@ public class Main {
     }
 
     // TODO: 3
-    // Retrieve Audit history data using OData Web API.
-    // This URL /api/data/v9.1/audits This provides the overall summary. But we need the individual
-    // Settings > System > Auditing turn it on
-    // Make sure user has the proper audit security roles Core Records > Miscellaneous Privileges
-    // View Audit History - View Audit Summary - View Audit Partitions - Delete Audit Partitions
     public static void readEntityAuditHistory(String accessToken) {
         class DynamicsEntityAudit {
             private String mChangeDate; // createdon
@@ -282,37 +259,28 @@ public class Main {
     }
 
     // TODO: 4
-    // Associate:
-    // How to avoid multiple OData Web API service call?
-    // Functional Scenario:
-    // - We have Account entity and Address entity.
-    // - We need to associate multiple address (more than 2 address) to a particular Account record.
-    // - We have created N:1 relationship between Address entity and Account entity.
-    public static void associateAccounts(String accessToken) {
-//        try {
-//            System.out.println("end");
-//        }
-//        catch (IOException e) {}
-    }
-    // - We have created one sub-grid for address entity using related record and placed that in Account entity.
-    public static void associateAccountSubGrid(String accessToken) {
-//        try {
-//            System.out.println("end");
-//        }
-//        catch (IOException e) {}
-    }
-    // - Consider we associated 5 addresses to one account record.
-    // - We want to retrieve these 5 records with respect to one single account.
-    // In that case we are executing the service call 5 times, which leads to performance issue.
-    public static void associatedAccountAddresses(String accessToken) {
-//        try {
-//            System.out.println("end");
-//        }
-//        catch (IOException e) {}
+    public static void getAssociatedAccountAddresses(String accessToken) {
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            String accountId = "da084227-2f4b-e911-a830-000d3a1d5a4d";
+
+            Request request = new Request.Builder()
+                    .url(REST_API_URL + "accounts%28" + accountId + "%29/Account_CustomerAddress")
+                    .get()
+                    .addHeader("OData-MaxVersion", "4.0")
+                    .addHeader("OData-Version", "4.0")
+                    .addHeader("Authorization", "Bearer " + accessToken)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            String dataReturnedFromGetAddresses = response.body().string();
+            System.out.println("end");
+        }
+        catch (IOException e) {}
     }
 
     // TODO: 5
-    // Retrieving customized responses on POST method:
     public static void createWithDataReturned(String accessToken) {
         try {
             OkHttpClient client = new OkHttpClient();
@@ -353,30 +321,9 @@ public class Main {
             OkHttpClient client = new OkHttpClient();
 
             MediaType mediaType = MediaType.parse("multipart/mixed;boundary=changeset_BBB456");
-            RequestBody body = RequestBody.create(mediaType, "--batch_AAA123\r\n" +
-                    "Content-Type: multipart/mixed;boundary=changeset_BBB456\r\n\r\n" +
-                    "--changeset_BBB456\r\nContent-Type: application/http\r\n" +
-                    "Content-Transfer-Encoding:binary\r\n" +
-                    "Content-ID: 1\r\n\r\n" +
-                    "POST https://msott.api.crm.dynamics.com/api/data/v9.0/accounts HTTP/1.1\r\n" +
-                    "Content-Type: application/json;type=entry\r\n\r\n" +
-                    "{\"name\":\"batch95349\"}\r\n\r\n" +
-                    "--changeset_BBB456\r\n" +
-                    "Content-Type: application/http\r\n" +
-                    "Content-Transfer-Encoding:binary\r\nContent-ID: 2\r\n\r\n" +
-                    "POST https://msott.api.crm.dynamics.com/api/data/v9.0/accounts HTTP/1.1\r\n" +
-                    "Content-Type: application/json;type=entry\r\n\r\n" +
-                    "{\"name\":\"batch95349\"}\r\n\r\n" +
-                    "--changeset_BBB456\r\n" +
-                    "Content-Type: application/http\r\nContent-Transfer-Encoding:binary\r\n" +
-                    "Content-ID: 3\r\n\r\n" +
-                    "POST https://msott.api.crm.dynamics.com/api/data/v9.0/accounts HTTP/1.1\r\n" +
-                    "Content-Type: application/json;type=entry\r\n\r\n" +
-                    "{\"name\":\"batch95375469\"}\r\n\r\n" +
-                    "--changeset_BBB456--\r\n--" +
-                    "batch_AAA123--");
+            RequestBody body = RequestBody.create(mediaType, "--batch_AAA123\r\nContent-Type: multipart/mixed;boundary=changeset_BBB456\r\n\r\n--changeset_BBB456\r\nContent-Type: application/http\r\nContent-Transfer-Encoding:binary\r\nContent-ID: 1\r\n\r\nPOST https://msott.api.crm.dynamics.com/api/data/v9.0/accounts HTTP/1.1\r\nContent-Type: application/json;type=entry\r\n\r\n{\"name\":\"batchgggg\"}\r\n\r\n--changeset_BBB456\r\nContent-Type: application/http\r\nContent-Transfer-Encoding:binary\r\nContent-ID: 2\r\n\r\nPOST https://msott.api.crm.dynamics.com/api/data/v9.0/accounts HTTP/1.1\r\nContent-Type: application/json;type=entry\r\n\r\n{\"name\":\"batchggggg49\"}\r\n\r\n--changeset_BBB456\r\nContent-Type: application/http\r\nContent-Transfer-Encoding:binary\r\nContent-ID: 3\r\n\r\nPOST https://msott.api.crm.dynamics.com/api/data/v9.0/accounts HTTP/1.1\r\nContent-Type: application/json;type=entry\r\n\r\n{\"name\":\"batch9gggg469\"}\r\n\r\n--changeset_BBB456--\r\n--batch_AAA123--");
             Request request = new Request.Builder()
-                    .url(REST_API_URL + "https://msott.api.crm.dynamics.com/api/data/v9.0/$batch")
+                    .url("https://msott.api.crm.dynamics.com/api/data/v9.0/$batch")
                     .post(body)
                     .addHeader("Content-Type", "multipart/mixed;boundary=changeset_BBB456")
                     .addHeader("Accept", "application/json")
