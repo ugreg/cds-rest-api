52afff13d688effd65fdcfd7e5ab869c7e0c9d12
Greg Degruy <gregory.degruy@gmail.com>
2019-11-07 08:44:01 -0800

    + start of auth unit test + some mock logic ~ json post data

--- dynamicscrmapi/src/test/java/DynamicsDaoUnitTest.java
+++ dynamicscrmapi/src/test/java/DynamicsDaoUnitTest.java
@@ -1,3 +1,4 @@
+import com.microsoft.aad.adal4j.AuthenticationContext;
 import common.DynamicsDao;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -22,7 +23,9 @@ import java.util.AbstractMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.*;
 
@@ -40,11 +43,74 @@ public class DynamicsDaoUnitTest {
     public void shouldAuthenticateWithAccessToken() {
         String clientId;
         String clientsecret;
-        String service = "";
         String authenticationResult;
         String authenticationContext;
-        String future;
         String accessToken;
+
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        try {
+            AuthenticationContext context
+                    = new AuthenticationContext("s", true, service);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        ExecutorService threadpool = Executors.newFixedThreadPool(3);
+        FactorialCalculator task = new FactorialCalculator(1000);
+        Future<Long> future = threadpool.submit(task);
+
+        while(!future.isDone()) {
+            System.out.println("Task is not completed yet....");
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Task is completed, let's check result");
+        long factorial = 0;
+        try {
+            factorial = future.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Factorial of 1000000 is : " + factorial);
+        threadpool.shutdown();
+    }
+
+    private static class FactorialCalculator implements Callable {
+        private final int number;
+
+        public FactorialCalculator(int number) {
+            this.number = number;
+        }
+
+        @Override
+        public Long call() {
+            long output = 0;
+            try {
+                output = factorial(number);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return output;
+        }
+
+        private long factorial(int number) throws InterruptedException {
+            if (number < 0) {
+                throw new IllegalArgumentException("Number must be greater than zero");
+            }
+            long result = 1;
+            while (number > 0) {
+                Thread.sleep(1);
+                result = result * number;
+                number--;
+            }
+            return  result;
+        }
     }
 
     @Test
